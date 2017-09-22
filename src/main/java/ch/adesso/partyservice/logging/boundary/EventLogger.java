af939678d5b0cb8c7380ddacc678e4471681f4c7
Robert Brem <brem_robert@hotmail.com>
2017-09-22 12:20:30 +0200

    refactored logging

---
+++ src/main/java/ch/adesso/partyservice/logging/boundary/EventLogger.java
@@ -0,0 +1,27 @@
+package ch.adesso.partyservice.logging.boundary;
+
+import ch.adesso.partyservice.party.event.CoreEvent;
+import ch.adesso.partyservice.party.event.PersonCreatedEvent;
+
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.event.Observes;
+import java.util.logging.Logger;
+
+@Startup
+@Singleton
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
+public class EventLogger {
+
+    private static final Logger LOG = Logger.getLogger(EventLogger.class.getName());
+
+    public void logEvent(@Observes CoreEvent event) {
+        LOG.info("Event: " + event + " sucessfully processed.");
+    }
+
+    public void logEvent(@Observes PersonCreatedEvent event) {
+        LOG.info("Person created, ID: " + event.getAggregateId() + " Version: " + event.getSequence());
+    }
+}
